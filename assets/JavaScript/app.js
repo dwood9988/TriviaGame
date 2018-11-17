1ce475baca476e1ef86c3fe669a9625acc2d80dd
Danielle Wood <danielle.wood9988@gmail.com>
2018-11-17 17:41:30 -0500

    Added trivia questions in the HTML file

---
+++ assets/JavaScript/app.js
@@ -0,0 +1,16 @@
+//create global variables 
+var questions = [
+
+
+    
+]
+//need a on click that runs the start game function
+//set time out to 30 seconds per questions (do that math)
+//set arrays for each questions with the possible answers
+//create buttons (bootstrap or <button> in query) for the multiple choice answers 
+//make sure they are only allowed to register one answer
+//create a for loop that will traverse that array to compare the users choice to the correct answer
+//separate array for the correct anwers? maybe use the [i] to see if user guess matches a certain [i]?
+//create gif that appears on a correct guess and if time create a separate one for an incorrect guess
+//append html at the end with the nuber of correct guesses, incorrect guesses, and unanswered questions.
+
