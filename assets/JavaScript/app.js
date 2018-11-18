d3787e4bbb1388a080d61558ed5b7a246f34b4af
Danielle Wood <danielle.wood9988@gmail.com>
2018-11-17 19:06:36 -0500

    added basic functionality and onclick event to confirm on submit is working

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,16 +1,21 @@
 //create global variables 
-var questions = [
+//need a on submit function that submits answers 
+//set time out to 30 seconds per questions (5 total minutes...is that too long?)
 
-
-    
-]
-//need a on click that runs the start game function
-//set time out to 30 seconds per questions (do that math)
 //set arrays for each questions with the possible answers
+//---done in the html---\\
 //create buttons (bootstrap or <button> in query) for the multiple choice answers 
+//---done in the html---\\
 //make sure they are only allowed to register one answer
+//---done in the html---\\
+
 //create a for loop that will traverse that array to compare the users choice to the correct answer
 //separate array for the correct anwers? maybe use the [i] to see if user guess matches a certain [i]?
-//create gif that appears on a correct guess and if time create a separate one for an incorrect guess
-//append html at the end with the nuber of correct guesses, incorrect guesses, and unanswered questions.
+//---if there's time---create gif that appears on a correct guess and if time create a separate one for an incorrect guess
+//append html (instructions div? somewhere at the bottom. maybe to replace the my-4) at the end with the nuber of correct guesses, incorrect guesses, and unanswered questions.
+//create a button (new element?) with an onclick event that resets the questions, timer, and my-4 to the original text. 
+//---someday - set up an API to bring in new questions? I'd have to rework all my html, but go back and do this after graduation---\\
 
+function onSubmit(){
+alert("It's Working!")
+}
